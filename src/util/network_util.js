6566b5d950f56e3ce020e09102129860417a548c
Sumit Mukhija <sumitmukhija@hotmail.com>
2019-12-17 13:36:59 +0000

    Added jest, changed style, added node modules to gitignore

---
+++ src/util/network_util.js
@@ -0,0 +1,7 @@
+export const urlCreater = (BASE_URL, API_KEY, latitude = 53.3498, longitude = 6.2603, RADIUS = 10000, type = "bar") =>
+{
+    if(!API_KEY || !BASE_URL){
+        // TODO: Return error
+    }
+    return BASE_URL+'/json?key='+API_KEY+'&location='+latitude+","+longitude+"&radius="+RADIUS+"&opennow&type="+type;
+}
