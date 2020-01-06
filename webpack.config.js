03873349d6856083195f2f78a5ebb8b14fa9d219
Animesh <hello@animesh.ltd>
2020-01-06 23:03:33 +0000

    Add dependencies and setup configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 const path = require('path');
 
 module.exports = {
-    "entry": "./src/app.js",
-    "output": {
-        filename: "bundle.js",
-        path: path.join(__dirname, "public/dist")
+  mode: "development",
+
+  // Enable sourcemaps for debugging webpack's output .js
+  devtool: "source-map",
+
+  // Add .ts and .tsx as resolvable extensions
+  resolve: {
+    extensions: [".ts", ".tsx"]
   },
+
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     compress: false,
     port: 8080
   },
+
   module: {
     rules: [
       { test: /\.(jpg|png)$/, use: { loader: 'url-loader' } },
@@ -20,6 +26,19 @@ module.exports = {
           loader: "sass-loader"
         }]
       },
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [{ loader: "ts-loader" }]
+      },
+      // All output '.js' files from TypeScript compiler will have any source
+      // maps reprocessed by 'source-map-loader'
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
+  },
+  
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
   }
 };
